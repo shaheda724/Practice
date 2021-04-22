c8705c327ddd9c3fb16133dad46ff3da52e5f538
shaheda724 <shaheeda.sony@gamil.com>
2021-04-22 13:20:23 +0530

    Added Test2 Component

---
+++ src/app/test2-component/test2-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test2-component',
+  templateUrl: './test2-component.component.html',
+  styleUrls: ['./test2-component.component.scss']
+})
+export class Test2ComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
